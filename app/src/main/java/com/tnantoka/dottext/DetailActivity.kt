c6c0900d5b90e567d2b4dbd62fa331ca2cd0a830
tnantoka <tnantoka@bornneet.com>
2022-11-06 12:52:54 +0900

    feat: back from detail activity

--- app/src/main/java/com/tnantoka/dottext/DetailActivity.kt
+++ app/src/main/java/com/tnantoka/dottext/DetailActivity.kt
@@ -1,11 +1,21 @@
 package com.tnantoka.dottext
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
