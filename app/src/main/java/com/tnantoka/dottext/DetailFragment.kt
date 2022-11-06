4fdd403a61be9a36a51775e4b8eccaee78d3e6a9
tnantoka <tnantoka@bornneet.com>
2022-11-06 23:50:23 +0900

    feat: edit file content

--- app/src/main/java/com/tnantoka/dottext/DetailFragment.kt
+++ app/src/main/java/com/tnantoka/dottext/DetailFragment.kt
@@ -1,14 +1,18 @@
 package com.tnantoka.dottext
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceManager
 import java.io.File
 
 class DetailFragment : Fragment(R.layout.fragment_detail) {
+    private var file: File? = null
+
     companion object {
         fun newInstance(file: File): DetailFragment {
             val fragment = DetailFragment()
@@ -24,9 +28,14 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
 
         val contentEdit = view.findViewById<EditText>(R.id.contentEdit)
 
+        contentEdit.requestFocus()
+        contentEdit.addTextChangedListener {
+            file?.writeText(it.toString())
+        }
+
         (arguments ?: activity?.intent?.extras)?.let {
-            val file = it.getSerializable("file") as File
-            contentEdit.setText(file.readText())
+            file = it.getSerializable("file") as File
+            contentEdit.setText(file?.readText())
         }
 
         context?.let {
